08d036242230420016a43850d4ba75988680b24c
Valerii Kan <valerii.kan1@gmail.com>
2019-03-12 16:59:50 +0200

    Aware components set up

---
+++ app/src/main/java/com/aware/app/stop_myo/model/Provider.java
@@ -0,0 +1,254 @@
+package com.aware.app.stop_myo.model;
+
+import android.content.ContentProvider;
+import android.content.ContentResolver;
+import android.content.ContentUris;
+import android.content.ContentValues;
+import android.content.Context;
+import android.content.UriMatcher;
+import android.database.Cursor;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteQueryBuilder;
+import android.net.Uri;
+import android.provider.BaseColumns;
+import android.support.annotation.Nullable;
+
+import com.aware.utils.DatabaseHelper;
+
+import java.util.HashMap;
+
+public class Provider extends ContentProvider {
+
+    /**
+     * Authority of this content provider
+     */
+    public static String AUTHORITY = "com.aware.app.stop_myo.model.provider.stop_myo";
+
+    /**
+     * ContentProvider database version. Increment every time you modify the database structure
+     */
+    public static final int DATABASE_VERSION = 1;
+
+    /**
+     * Database stored in external folder: /package.name/stop_myo.db
+     */
+    public static final String DATABASE_NAME = "stop_myo.db";
+
+    //Database table names
+    public static final String DB_TBL_MYO = "myo";
+
+    //ContentProvider query indexes
+    private static final int TABLE_MYO_DIR = 1;
+    private static final int TABLE_MYO_ITEM = 2;
+
+    /**
+     * Database tables:
+     * - myo data
+     */
+    public static final String[] DATABASE_TABLES = {
+            DB_TBL_MYO
+    };
+
+    //These are columns that we need to sync data, don't change this!
+    public interface AWAREColumns extends BaseColumns {
+        String _ID = "_id";
+        String TIMESTAMP = "timestamp";
+        String DEVICE_ID = "device_id";
+    }
+
+    /**
+     * Myo data table
+     */
+    public static final class Myo_Data implements AWAREColumns {
+        public static final Uri CONTENT_URI = Uri.parse("content://" + AUTHORITY + "/" + DB_TBL_MYO);
+        public static final String CONTENT_TYPE = ContentResolver.CURSOR_DIR_BASE_TYPE + "/vnd.com.aware.app.stop_myo.model.provider.myo";
+        public static final String CONTENT_ITEM_TYPE = ContentResolver.CURSOR_ITEM_BASE_TYPE + "/vnd.com.ubicomp.stopit.stopit.model.provider.myo";
+
+        public static final String DATA = "data";
+    }
+
+    //Myo table fields
+    private static final String DB_TBL_MYO_FIELDS =
+            Myo_Data._ID + " integer primary key autoincrement," +
+                    Myo_Data.TIMESTAMP + " real default 0," +
+                    Myo_Data.DEVICE_ID + " text default ''," +
+                    Myo_Data.DATA + " longtext default ''";
+
+
+    /**
+     * Share the fields with AWARE so we can replicate the table schema on the server
+     */
+    public static final String[] TABLES_FIELDS = {
+            DB_TBL_MYO_FIELDS
+    };
+
+    //Helper variables for ContentProvider - DO NOT CHANGE
+    private UriMatcher sUriMatcher;
+    private DatabaseHelper dbHelper;
+    private static SQLiteDatabase database;
+    private void initialiseDatabase() {
+        if (dbHelper == null)
+            dbHelper = new DatabaseHelper(getContext(), DATABASE_NAME, null, DATABASE_VERSION, DATABASE_TABLES, TABLES_FIELDS);
+        if (database == null)
+            database = dbHelper.getWritableDatabase();
+    }
+
+    //For each table, create a hashmap needed for database queries
+    private HashMap<String, String> tableMyoHash;
+
+    /**
+     * Returns the provider authority that is dynamic
+     * @return
+     */
+    public static String getAuthority(Context context) {
+        AUTHORITY = context.getPackageName() + ".model.provider.stop_myo";
+        return AUTHORITY;
+    }
+
+    @Override
+    public boolean onCreate() {
+        //This is a hack to allow providers to be reusable in any application/plugin by making the authority dynamic using the package name of the parent app
+        AUTHORITY = getContext().getPackageName() + ".model.provider.stop_myo";
+
+        sUriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
+
+        //Myo table indexes DIR and ITEM
+        sUriMatcher.addURI(AUTHORITY, DATABASE_TABLES[0], TABLE_MYO_DIR);
+        sUriMatcher.addURI(AUTHORITY, DATABASE_TABLES[0] + "/#", TABLE_MYO_ITEM);
+
+        //Myo table HasMap
+        tableMyoHash = new HashMap<>();
+        tableMyoHash.put(Myo_Data._ID, Myo_Data._ID);
+        tableMyoHash.put(Myo_Data.TIMESTAMP, Myo_Data.TIMESTAMP);
+        tableMyoHash.put(Myo_Data.DEVICE_ID, Myo_Data.DEVICE_ID);
+        tableMyoHash.put(Myo_Data.DATA, Myo_Data.DATA);
+
+        return true;
+    }
+
+    @Override
+    public int delete(Uri uri, String selection, String[] selectionArgs) {
+        initialiseDatabase();
+
+        database.beginTransaction();
+
+        int count;
+        switch (sUriMatcher.match(uri)) {
+
+            case TABLE_MYO_DIR:
+                count = database.delete(DATABASE_TABLES[0], selection, selectionArgs);
+                break;
+
+            default:
+                database.endTransaction();
+                throw new IllegalArgumentException("Unknown URI " + uri);
+        }
+
+        database.setTransactionSuccessful();
+        database.endTransaction();
+
+        getContext().getContentResolver().notifyChange(uri, null, false);
+        return count;
+    }
+
+    @Nullable
+    @Override
+    public Uri insert(Uri uri, ContentValues initialValues) {
+
+        initialiseDatabase();
+
+        ContentValues values = (initialValues != null) ? new ContentValues(initialValues) : new ContentValues();
+
+        database.beginTransaction();
+
+        switch (sUriMatcher.match(uri)) {
+
+            case TABLE_MYO_DIR:
+                long myo_id = database.insert(DATABASE_TABLES[0], Myo_Data.DEVICE_ID, values);
+                database.setTransactionSuccessful();
+                database.endTransaction();
+                if (myo_id > 0) {
+                    Uri dataUri = ContentUris.withAppendedId(Myo_Data.CONTENT_URI, myo_id);
+                    getContext().getContentResolver().notifyChange(dataUri, null, false);
+                    return dataUri;
+                }
+                database.endTransaction();
+                throw new SQLException("Failed to insert row into " + uri);
+
+            default:
+                database.endTransaction();
+                throw new IllegalArgumentException("Unknown URI " + uri);
+        }
+    }
+
+    @Nullable
+    @Override
+    public Cursor query(Uri uri, String[] projection, String selection, String[] selectionArgs, String sortOrder) {
+
+        initialiseDatabase();
+
+        SQLiteQueryBuilder qb = new SQLiteQueryBuilder();
+        switch (sUriMatcher.match(uri)) {
+
+            case TABLE_MYO_DIR:
+                qb.setTables(DATABASE_TABLES[0]);
+                qb.setProjectionMap(tableMyoHash); //the hashmap of the table
+                break;
+
+            default:
+                throw new IllegalArgumentException("Unknown URI " + uri);
+        }
+
+        //Don't change me
+        try {
+            Cursor c = qb.query(database, projection, selection, selectionArgs,
+                    null, null, sortOrder);
+            c.setNotificationUri(getContext().getContentResolver(), uri);
+            return c;
+        } catch (IllegalStateException e) {
+            return null;
+        }
+    }
+
+    @Nullable
+    @Override
+    public String getType(Uri uri) {
+        switch (sUriMatcher.match(uri)) {
+
+            case TABLE_MYO_DIR:
+                return Myo_Data.CONTENT_TYPE;
+            case TABLE_MYO_ITEM:
+                return Myo_Data.CONTENT_ITEM_TYPE;
+
+            default:
+                throw new IllegalArgumentException("Unknown URI " + uri);
+        }
+    }
+
+    @Override
+    public int update(Uri uri, ContentValues values, String selection, String[] selectionArgs) {
+
+        initialiseDatabase();
+
+        database.beginTransaction();
+
+        int count;
+        switch (sUriMatcher.match(uri)) {
+
+            case TABLE_MYO_DIR:
+                count = database.update(DATABASE_TABLES[0], values, selection, selectionArgs);
+                break;
+
+            default:
+                database.endTransaction();
+                throw new IllegalArgumentException("Unknown URI " + uri);
+        }
+        database.setTransactionSuccessful();
+        database.endTransaction();
+
+        getContext().getContentResolver().notifyChange(uri, null, false);
+
+        return count;
+    }
+}
---
+++ app/src/main/java/com/aware/app/stop_myo/model/Stop_Myo_Sync.java
@@ -0,0 +1,38 @@
+package com.aware.app.stop_myo.model;
+
+import android.app.Service;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+
+import com.aware.syncadapters.AwareSyncAdapter;
+
+public class Stop_Myo_Sync extends Service {
+    private AwareSyncAdapter sSyncAdapter = null;
+    private static final Object sSyncAdapterLock = new Object();
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        synchronized (sSyncAdapterLock) {
+
+            if (sSyncAdapter == null) {
+                sSyncAdapter = new AwareSyncAdapter(getApplicationContext(), true, true);
+                sSyncAdapter.init(
+                        Provider.DATABASE_TABLES, Provider.TABLES_FIELDS,
+                        new Uri[]{
+                                Provider.Myo_Data.CONTENT_URI,
+                        }
+                );
+            }
+        }
+    }
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return sSyncAdapter.getSyncAdapterBinder();
+    }
+}
+
